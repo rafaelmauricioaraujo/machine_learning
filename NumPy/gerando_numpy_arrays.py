742e4275e576ff489dc7c9c07f13bde2e1a380c8
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-03 15:30:03 -0300

    criando array de zeros, espeficicando o tipo

--- NumPy/gerando_numpy_arrays.py
+++ NumPy/gerando_numpy_arrays.py
@@ -7,6 +7,9 @@ print(np.array(minha_lista).dtype)
 print('criando um array de zeros com função build-in numpy')
 zero_array = np.zeros((3, 4))
 print(zero_array)
+print(zero_array.dtype)
 
-
-
+print('também é possível espeficicar o tipo dos elementos do array')
+zero_array_int = np.zeros((3, 4), int)
+print(zero_array_int)
+print(zero_array_int.dtype)
