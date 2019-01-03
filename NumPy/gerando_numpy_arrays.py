872321ffdb348ee7660cca56f5dfc621f386ead1
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-03 15:27:38 -0300

    utilizando funções buil-in do NumPy para criar arrays

---
+++ NumPy/gerando_numpy_arrays.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+minha_lista = [1, 3, 5, 7, 9]
+print(np.array(minha_lista))
+print(np.array(minha_lista).dtype)
+
+print('criando um array de zeros com função build-in numpy')
+zero_array = np.zeros((3, 4))
+print(zero_array)
+
