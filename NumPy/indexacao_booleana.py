220fd5405ad5a8c2398f1e6e204c72b10f9ef021
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-03 19:01:16 -0300

    funções de conjuntos build-in do numpy

--- NumPy/indexacao_booleana.py
+++ NumPy/indexacao_booleana.py
@@ -12,4 +12,13 @@ print(X[(X < 10) & (X >= 7)])
 (X[(X < 5) & (X >= 0)]) = 1
 print(X)
 
+# funções de conjunto usando array
+
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([6, 7, 2, 8, 4])
+
+print(np.intersect1d(x, y))
+print(np.setdiff1d(x, y))
+print(np.union1d(x, y))
+
 
