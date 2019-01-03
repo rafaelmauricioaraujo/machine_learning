29c0a31e9508831e551e3f9897bc40479b55cf14
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-03 10:48:46 -0300

    testando conceitos de underfitting e overfitting com sklearn

--- scikit_learn/underfitting_overfitting.py
+++ scikit_learn/underfitting_overfitting.py
@@ -1,26 +1,25 @@
 # Import, read, and split data
 import pandas as pd
-data = pd.read_csv('data2.csv')
 import numpy as np
+# Imports
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.svm import SVC
+
+data = pd.read_csv('data2.csv')
 X = np.array(data[['x1', 'x2']])
 y = np.array(data['y'])
 
 # Fix random seed
 np.random.seed(55)
 
-### Imports
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.svm import SVC
-
 # TODO: Uncomment one of the three classifiers, and hit "Test Run"
-# to see the learning curve. Use these to answer the quiz below.
 
-### Logistic Regression
-#estimator = LogisticRegression()
+# Logistic Regression
+estimator = LogisticRegression()
 
-### Decision Tree
-#estimator = GradientBoostingClassifier()
+# Decision Tree
+# estimator = GradientBoostingClassifier()
 
-### Support Vector Machine
-#estimator = SVC(kernel='rbf', gamma=1000)
+# Support Vector Machine
+# estimator = SVC(kernel='rbf', gamma=1000)
