3e0db5bae4bdd49c46f2d3aac88f23ae70ffc608
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-03 17:08:05 -0300

    acessando numpy arrays com índices positivos e negativos

---
+++ NumPy/editando_numpy_arrays.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+array = np.array([1, 2, 3, 4, 5])
+print(array)
+
+print('Acessando elementos pelos índices')
+
+print('array[0]: ', array[0])
+print('array[1]: ', array[1])
+print('array[4]: ', array[4])
+
+print('Também é possível espeficifar índices negativos para acessar a partir do final')
+
+print('array[-5]: ', array[-5])
+print('array[-4]: ', array[-4])
+print('array[-1]: ', array[-1])
