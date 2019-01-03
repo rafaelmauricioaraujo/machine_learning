66badd282eed998be7efbd327dd6b9652f13ce09
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-03 18:27:45 -0300

    fatiando ndarrays

--- NumPy/fatiando_arrays.py
+++ NumPy/fatiando_arrays.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def pula_linha():
-    print('\n')
+    print('')
 
 
 def mostra(parametro):
@@ -22,11 +22,23 @@ Z = X[1:4, 2:5]
 mostra(Z)
 
 # outra maneira:
+# linhas: [1 até o final], colunas [2 até o final]
 W = X[1:, 2:]
 mostra(W)
 
 # outra seleção:
+# linhas: [inicio até o a segunda], colunas [2 até o final]
 J = X[:3, 2:]
 mostra(J)
+
+# linhas: [todas], colunas [terceira]
+R = X[:, 2]
+mostra(R)
+
+# agora o mesmo mas em uma coluna
+H = X[:, 2:3]
+mostra(H)
+
+
 
 
