60cc87a733f175695308a5aed3eaf1a0f3ad9644
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-03 15:13:16 -0300

    características dos arrays NumPy

--- NumPy/criando_e_salvando_array_np.py
+++ NumPy/criando_e_salvando_array_np.py
@@ -29,3 +29,39 @@ print(array_2d.shape)
 tamanho_array_2d = array_2d.shape
 print('O tamanho do array é: {}'.format(tamanho_array_2d))
 print('o tipo do da variável é: {}'.format(type(tamanho_array_2d)))
+
+# a dimensão do array é referenciado pelo rank
+# exemplo: um array de rank = 1:
+strings = np.array(['Hello', 'World'])
+
+# imprimindo o array
+print('array de strings: ')
+print(strings)
+
+# imprimindos suas características
+
+print(type(strings))
+print(strings.shape)
+print(strings.dtype)
+print("este formato: '{}' é Unicode com 5 caracteres".format(strings.dtype))
+
+# O arrays do NumPy sempre são do mesmo tipo, até quando é criado array com tipos variádos, o NumPy os converte
+
+print('array de tipos variados')
+
+tipos_variados = np.array(['Rafael', 1, 2.3, 'Teti'])
+print(tipos_variados)
+
+print('Suas caracteristicas: ')
+print(type(tipos_variados))
+print(tipos_variados.shape)
+print(tipos_variados.dtype)
+
+# é possível espeficicar o tipo de dado a ser preenchido no array, usado por questões de segurança ou performace
+
+print('array espeficicando o tipo')
+
+array_float = np.array([1.0, 2.2, 3.8], dtype=int)
+print(array_float)
+
+
