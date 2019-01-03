9c58be16b699720b73002805b6b791bcb8cbf651
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-03 19:05:27 -0300

    funções de conjuntos build-in do numpy

--- NumPy/indexacao_booleana.py
+++ NumPy/indexacao_booleana.py
@@ -21,4 +21,52 @@ print(np.intersect1d(x, y))
 print(np.setdiff1d(x, y))
 print(np.union1d(x, y))
 
+# Vamos criar uma ndarray desordenada de ordem 1
+x = np.random.randint(1, 11, size=(10,))
 
+# Vamos exibir o x
+print()
+print('Original x = ', x)
+
+# Ordenamos x e exibimos a array ordenada após usar `sort` como uma função.
+print()
+print('Sorted x (out of place):', np.sort(x))
+
+# Quando ordenamos fora do lugar, a array original permanece intacta. Para ver isso, podemos imprimir x novamente
+print()
+print('x after sorting:', x)
+
+# Ordenamos x mas mantendo apenas elementos únicos
+print()
+print(np.sort(np.unique(x)))
+
+# Vamos criar uma ndarray desordenada de ordem 1
+x = np.random.randint(1, 11, size=(10,))
+
+# Vamos exibir o x
+print()
+print('Original x = ', x)
+
+# Ordenamos x e exibimos a array ordenada após usar `sort` como um método.
+x.sort()
+
+# Quando usamos o sort como método da instância de ndarray, a array original é alterada para uma array ordenada.
+# Para ver isso, podemos imprimir x novamente
+print()
+print('x after sorting:', x)
+
+# Vamos criar uma ndarray desordenada de ordem 2
+X = np.random.randint(1, 11, size=(5, 5))
+
+# Vamos exibir o X
+print()
+print('Original X = \n', X)
+print()
+
+# Ordenamos X por suas colunas e exibimos a array ordenada
+print()
+print('X with sorted columns :\n', np.sort(X, axis=0))
+
+# Ordenamos X por suas linhas e exibimos a array ordenada
+print()
+print('X with sorted rows :\n', np.sort(X, axis=1))
