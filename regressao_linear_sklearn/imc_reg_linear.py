036a308171feee4e3cc3037bc4cbf5f859051d1e
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-07 23:52:46 -0300

    criando um modelo de regressão linear e alimentando com dados

--- regressao_linear_sklearn/imc_reg_linear.py
+++ regressao_linear_sklearn/imc_reg_linear.py
@@ -5,7 +5,17 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
+# importando dados para alimentar o modelo
 bmi_life_data = pd.read_csv('bmi_and_life_expectancy.csv')
 print(bmi_life_data.shape)
 print(bmi_life_data.head())
+
+# criando um modelo
+bmi_life_model = LinearRegression()
+bmi_life_model.fit(bmi_life_data['BMI'], bmi_life_data['Life expectancy'])
+
+
+
+
+
 
