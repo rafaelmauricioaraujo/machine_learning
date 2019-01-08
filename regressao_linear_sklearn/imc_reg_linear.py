d1ad06ad886d71be3be0824e3194d77d7b689839
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-07 23:28:59 -0300

    importando a biblioteca de regressão linear do sklearn

--- regressao_linear_sklearn/imc_reg_linear.py
+++ regressao_linear_sklearn/imc_reg_linear.py
@@ -1,2 +1,5 @@
 # Utilizando a biblioteca de regressão linear do scikit learn para prever a expectativa de vida a partir do
 # índice de massa corporal: IMC.
+
+from sklearn.linear_model import LinearRegression
+
