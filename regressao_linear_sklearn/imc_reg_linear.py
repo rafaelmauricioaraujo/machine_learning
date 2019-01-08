e713a66b734f40db5f9fc9d30fa61f76abf96665
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-07 23:37:25 -0300

    carregando dados de expectativa de vida com pandas

--- regressao_linear_sklearn/imc_reg_linear.py
+++ regressao_linear_sklearn/imc_reg_linear.py
@@ -1,8 +1,11 @@
 # Utilizando a biblioteca de regressão linear do scikit learn para prever a expectativa de vida a partir do
 # índice de massa corporal: IMC.
 
+# imports
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-model = LinearRegression()
+bmi_life_data = pd.read_csv('bmi_and_life_expectancy.csv')
+print(bmi_life_data.shape)
+print(bmi_life_data.head())
 
-model.fit(x_values, y_values)
