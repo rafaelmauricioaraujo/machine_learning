5b08176cd519b35abd0fa60b978144e459994614
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-07 23:59:21 -0300

    fazendo previsões

--- regressao_linear_sklearn/imc_reg_linear.py
+++ regressao_linear_sklearn/imc_reg_linear.py
@@ -7,14 +7,14 @@ from sklearn.linear_model import LinearRegression
 
 # importando dados para alimentar o modelo
 bmi_life_data = pd.read_csv('bmi_and_life_expectancy.csv')
-print(bmi_life_data.shape)
-print(bmi_life_data.head())
 
 # criando um modelo
 bmi_life_model = LinearRegression()
-bmi_life_model.fit(bmi_life_data['BMI'], bmi_life_data['Life expectancy'])
+bmi_life_model.fit(bmi_life_data[['BMI']], bmi_life_data[['Life expectancy']])
 
-
+# fazendo previsões
+laos_life_exp = bmi_life_model.predict([21.07931])
+print(laos_life_exp)
 
 
 
