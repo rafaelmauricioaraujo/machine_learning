4056a4c0b0814280632b0052be7debbfe97ad5c0
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-11 22:06:02 -0300

    alguns palavras sobre o Pandas

---
+++ Pandas/pandas_testes.py
@@ -0,0 +1,4 @@
+# Pandas se trata de um pacote de manipulação de dados e análise em Python. O nome Pandas é derivado
+# do termo econométrico painel de dados. O pandas incorpora duas estruturas de dados adicionais ao Python,
+# ou seja, Séries Pandas e DataFrame pandas.
+
