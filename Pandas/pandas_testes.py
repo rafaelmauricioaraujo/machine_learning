73adbc3e5021fcaccf345fc85faab86dc6a45099
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-11 22:09:41 -0300

    criando imprimindo uma série com Pandas

--- Pandas/pandas_testes.py
+++ Pandas/pandas_testes.py
@@ -5,3 +5,8 @@
 # importando pandas
 import pandas as pd
 
+# Criando uma série do Pandas que armazena uma lista de compras
+lista_compras = pd.Series(data=[30, 6, 'Yes', 'No'], index=['Ovos', 'Maças', 'Leite', 'Pão'])
+
+# Exibindo lista
+print(lista_compras)
