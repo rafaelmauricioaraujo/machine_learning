66f4e3b73dbc866c9eb8d18036b922dfb3149a61
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-13 18:35:55 -0300

    utilizando a mesma lista dos exemplos anteriores

---
+++ Pandas/pandas_acesso_exclusao.py
@@ -0,0 +1,9 @@
+# importando pandas
+import pandas as pd
+
+# Criando uma série do Pandas que armazena uma lista de compras
+lista_compras = pd.Series(data=[30, 6, 'Yes', 'No'], index=['Ovos', 'Maças', 'Leite', 'Pão'])
+
+# Exibindo lista
+print(lista_compras)
+
