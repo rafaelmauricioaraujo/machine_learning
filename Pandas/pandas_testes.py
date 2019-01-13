8d8d21ad02848554b025c80f6b1132fcfc56579e
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-13 18:32:26 -0300

    adicionando alguns comentários

--- Pandas/pandas_testes.py
+++ Pandas/pandas_testes.py
@@ -1,11 +1,12 @@
 # Pandas se trata de um pacote de manipulação de dados e análise em Python. O nome Pandas é derivado
 # do termo econométrico painel de dados. O pandas incorpora duas estruturas de dados adicionais ao Python,
-# ou seja, Séries Pandas e DataFrame pandas.
+# que são, Séries Pandas e DataFrame pandas.
 
 # importando pandas
 import pandas as pd
 
 # Criando uma série do Pandas que armazena uma lista de compras
+# Com pandas é possível espeficifar tanto o valores quanto os rótulos
 lista_compras = pd.Series(data=[30, 6, 'Yes', 'No'], index=['Ovos', 'Maças', 'Leite', 'Pão'])
 
 # Exibindo lista
