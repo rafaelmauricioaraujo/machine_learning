099b0f00c36065ee61fecb7a48830adcb8975a04
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-17 20:31:59 -0300

    operações aritméticas elemento a elemento

--- Pandas/pandas_oper_aritmeticas.py
+++ Pandas/pandas_oper_aritmeticas.py
@@ -2,3 +2,17 @@ import pandas as pd
 
 fruits = pd.Series([10, 6, 3], ['maças', 'laranjas', 'bananas'])
 
+# Exibindo a lista para referência
+print(fruits)
+
+# É possível executar operações básicas, elemento a elemento usando simbolos aritméticos
+print()
+print('frutas + 2 \n', fruits + 2)
+print()
+print('frutas - 2 \n', fruits - 2)
+print()
+print('frutas * 2 \n', fruits * 2)
+print()
+print('frutas / 2 \n', fruits / 2)
+
+
