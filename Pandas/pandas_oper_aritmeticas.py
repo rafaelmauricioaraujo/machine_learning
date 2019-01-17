bee23485d1c09be7c4d3962fddddc59cfdb3484d
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-17 20:36:03 -0300

    aplicando operações matemáticas do NumPy no pandas

--- Pandas/pandas_oper_aritmeticas.py
+++ Pandas/pandas_oper_aritmeticas.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 fruits = pd.Series([10, 6, 3], ['maças', 'laranjas', 'bananas'])
 
@@ -14,5 +15,21 @@ print()
 print('frutas * 2 \n', fruits * 2)
 print()
 print('frutas / 2 \n', fruits / 2)
+
+# Também é possível aplicar funções matemáticas do NumPy, tais como 'sqrt(x)' a todos os elementos
+
+# Exibindo a lista para referência
+print(fruits)
+
+# Aplicando diferentes funções matemáticas a todos os elementos
+print()
+print('EXP(X) = \n', np.exp(fruits))
+print()
+print('SQTR(X) = \n', np.sqrt(fruits))
+print()
+print('POW(X) = \n', np.power(fruits, 2))
+
+
+
 
 
