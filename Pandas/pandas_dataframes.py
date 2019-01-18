156251d56a9720fa4e6423122f993abd491efd20
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-17 22:45:50 -0300

    criando DataFrames do Pandas

--- Pandas/pandas_dataframes.py
+++ Pandas/pandas_dataframes.py
@@ -23,3 +23,13 @@ df = pd.DataFrame(data)
 # Exibindo  o DataFrame
 print(df)
 
+# Também é possível obter algumas informações sobre o carrinho de compras
+print('carrinho de compras.shape: ', carrinho_de_compras.shape)
+print('carrinho de compras.ndim: ', carrinho_de_compras.ndim)
+print('carrinho de compras tem um total de: ', carrinho_de_compras.size, 'elementos')
+print()
+print('os dados no carrinho de compras são: ', carrinho_de_compras.values)
+print()
+print('os indices das linhas no carrinho de compras são: ', carrinho_de_compras.index)
+print()
+print('os indices das colunas no carrinho de comrpas sao: ', carrinho_de_compras.columns)
