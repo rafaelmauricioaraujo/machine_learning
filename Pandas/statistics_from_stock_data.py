78ee7eff4613c2a30405d4c9207882844c8fb1f0
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-01-21 20:50:18 -0300

    carregando e configurando dados

--- Pandas/statistics_from_stock_data.py
+++ Pandas/statistics_from_stock_data.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import numpy as np
 
+# Load the Google stock data
 google_stock = pd.read_csv('GOOG.csv', index_col=['Date'], parse_dates=['Date'], usecols=['Date', 'Adj Close'])
+
+# Load the Apple stock data
+apple_stock = pd.read_csv('AAPL.csv', index_col=['Date'], parse_dates=['Date'], usecols=['Date', 'Adj Close'])
+
+# Load the Amazon stock data
+
+amazon_stock = pd.read_csv('AMZN.csv', index_col=['Date'], parse_dates=['Date'], usecols=['Date', 'Adj Close'])
 
 print(google_stock.head())
